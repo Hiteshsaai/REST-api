8ec4c3ed30af8095991b698764d00e9d3f007b52
Hiteshsaai <hiteshsaaimananchery@gmail.com>
2019-07-31 11:16:59 -0400

    made changes to item and store file

--- models/item.py
+++ models/item.py
@@ -1,7 +1,7 @@
 from db import db
 
 class ItemModel(db.Model):
-    ___tablename__ = 'items'
+    __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(80))
