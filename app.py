7f9e623c16c99c548514b028df9f559678b76938
Hiteshsaai <hiteshsaaimananchery@gmail.com>
2019-07-31 11:46:31 -0400

    Added POSTgres databased and psycopy2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -8,8 +8,8 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app = Flask(__name__)                                #It not this   #Then this
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'keys'
